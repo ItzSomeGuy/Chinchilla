a0a97071a2a4ed9840fa76bce1d6e6ef467e8214
ItzSomeGuy <a.floyd135@gmail.com>
2024-04-06 14:16:21 -0400

    bug fixes @,@

--- src/main/java/com/chipset/Listeners/ReactionListener.java
+++ src/main/java/com/chipset/Listeners/ReactionListener.java
@@ -3,6 +3,7 @@ package com.chipset.Listeners;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
@@ -11,26 +12,34 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ReactionListener extends ListenerAdapter {
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
-        Guild guild = event.getGuild();
-        Member member = event.getMember(); assert member != null;
-        ForumChannel forumChannel = guild.getForumChannelById(1062841778270642308L);
-        ThreadChannel threadChannel = event.getChannel().asThreadChannel(); assert forumChannel != null;
-        Message message = threadChannel.retrieveStartMessage().complete();
+        if (event.getChannel().getType() == ChannelType.GUILD_PUBLIC_THREAD) {
+            Guild guild = event.getGuild();
+            Member member = event.getMember();
+            assert member != null;
+            ForumChannel forumChannel = guild.getForumChannelById(1062841778270642308L);
+            ThreadChannel threadChannel = event.getChannel().asThreadChannel();
+            assert forumChannel != null;
+            Message message = threadChannel.retrieveStartMessage().complete();
 
-        if (threadChannel.getParentChannel().getIdLong() == forumChannel.getIdLong() && event.getMessageIdLong() == message.getIdLong()) {
-            threadChannel.addThreadMember(member).queue();
+            if (threadChannel.getParentChannel().getIdLong() == forumChannel.getIdLong() && event.getMessageIdLong() == message.getIdLong()) {
+                threadChannel.addThreadMember(member).queue();
+            }
         }
     }
 
     public void onMessageReactionRemove(MessageReactionRemoveEvent event) {
-        Guild guild = event.getGuild();
-        Member member = event.getMember(); assert member != null;
-        ForumChannel forumChannel = guild.getForumChannelById(1062841778270642308L);
-        ThreadChannel threadChannel = event.getChannel().asThreadChannel(); assert forumChannel != null;
-        Message message = threadChannel.retrieveStartMessage() .complete();
+        if (event.getChannel().getType() == ChannelType.GUILD_PUBLIC_THREAD) {
+            Guild guild = event.getGuild();
+            Member member = event.getMember();
+            assert member != null;
+            ForumChannel forumChannel = guild.getForumChannelById(1062841778270642308L);
+            ThreadChannel threadChannel = event.getChannel().asThreadChannel();
+            assert forumChannel != null;
+            Message message = threadChannel.retrieveStartMessage().complete();
 
-        if (threadChannel.getParentChannel().getIdLong() == forumChannel.getIdLong() && event.getMessageIdLong() == message.getIdLong()) {
-            threadChannel.removeThreadMember(member).queue();
+            if (threadChannel.getParentChannel().getIdLong() == forumChannel.getIdLong() && event.getMessageIdLong() == message.getIdLong()) {
+                threadChannel.removeThreadMember(member).queue();
+            }
         }
     }
 }
--- src/main/java/com/chipset/Listeners/ReadyListener.java
+++ src/main/java/com/chipset/Listeners/ReadyListener.java
@@ -51,122 +51,44 @@ public class ReadyListener extends ListenerAdapter {
         Guild guild = event.getJDA().getGuildById(193117152709050368L); assert guild != null;
 
         // Define the array of names
-        List<String> catBreeds = Arrays.asList(
-                // Officially recognized domestic cat breeds
-                "Abyssinian",
-                "American Bobtail",
-                "American Curl",
-                "American Shorthair",
-                "American Wirehair",
-                "Balinese",
-                "Bengal",
-                "Birman",
-                "Bombay",
-                "British Shorthair",
-                "Burmese",
-                "Chartreux",
-                "Cornish Rex",
-                "Devon Rex",
-                "Egyptian Mau",
-                "Exotic Shorthair",
-                "Havana Brown",
-                "Himalayan",
-                "Japanese Bobtail",
-                "Javanese",
-                "Korat",
-                "LaPerm",
-                "Maine Coon",
-                "Manx",
-                "Munchkin",
-                "Nebelung",
-                "Norwegian Forest Cat",
-                "Ocicat",
-                "Oriental",
-                "Persian",
-                "Peterbald",
-                "Pixie-bob",
-                "Ragamuffin",
-                "Ragdoll",
-                "Russian Blue",
-                "Scottish Fold",
-                "Selkirk Rex",
-                "Siamese",
-                "Siberian",
-                "Singapura",
-                "Snowshoe",
-                "Somali",
-                "Sphynx",
-                "Tonkinese",
-                "Toyger",
-                "Turkish Angora",
-                "Turkish Van",
-                "York Chocolate",
-                // Unofficially recognized breeds or mixed breeds
-                "Calico",
-                "Tabby",
-                "Tuxedo",
-                "Torbie",
-                "Domestic Shorthair",
-                "Domestic Longhair",
-                "Domestic Medium Hair",
-                // Designer breeds
-                "Bengal x Siamese",
-                "Maine Coon x Siamese",
-                "Persian x Siamese",
-                // Other popular mixes
-                "Siamese x Domestic",
-                "Maine Coon x American",
-                // Wildcat species
-                "African Wildcat",
-                "European Wildcat",
-                "Asian Wildcat",
-                "Black-footed Cat",
-                "Caracal",
-                "Ocelot",
-                "Serval",
-                // Related animals
-                "Lion",
-                "Tiger",
-                "Leopard",
-                "Jaguar",
-                "Cheetah",
-                "Lynx",
-                "Puma",
-                "Panther",
-                "Civet",
-                "Genet",
-                "Linsang",
-                "Fossa",
-                "Clouded Leopard",
-                "Snow Leopard",
-                "Bobcat",
-                "Cougar",
-                "Jungle Cat",
-                "Sand Cat",
-                "Marbled Cat",
-                "Pallas's Cat",
-                "Andean Mountain Cat",
-                "Iberian Lynx",
-                "Canadian Lynx",
-                // Additional wildcat species
-                "Bengalensis",
-                "Leptailurus",
-                "Felis margarita",
-                "Felis nigripes",
-                "Felis silvestris",
-                // Additional related animals
-                "Servaline Genet",
-                "Binturong",
-                "Tayra",
-                "Asian Golden Cat",
-                "Rusty-spotted Cat",
-                "Flat-headed Cat",
-                "Oncilla",
-                "Geoffroy's Cat",
-                "Bay Cat",
-                "Kodkod"
-                // Add more breeds, species, or related animals here if needed
-                // ...
+        List<String> wormTypes = Arrays.asList(
+                "Earthworm",
+                "Roundworm",
+                "Tapeworm",
+                "Hookworm",
+                "Flatworm",
+                "Bloodworm",
+                "Ragworm",
+                "Pinworm",
+                "Whipworm",
+                "Filarial worm",
+                "Guinea worm",
+                "Leech",
+                "Horsehair worm",
+                "Threadworm",
+                "Ribbon worm",
+                "Arrowworm",
+                "Velvet worm",
+                "Peanut worm",
+                "Beard worm",
+                "Spaghetti worm",
+                "Palolo worm",
+                "Eelworm",
+                "Vinegar eel",
+                "Arrowhead worm",
+                "Bristleworm",
+                "Sandworm",
+                "Moon jellyfish worm",
+                "Horsehair worm",
+                "Arrow worm",
+                "Beard worm",
+                "Horsehair worm",
+                "Blackworm",
+                "Mermithid worm",
+                "Gordian worm",
+                "Acorn worm",
+                "Arrow worm",
+                "Marine worm"
         );
 
         // Get the Member object for the specified member
@@ -177,10 +99,10 @@ public class ReadyListener extends ListenerAdapter {
             @Override
             public void run() {
                 // Generate a random index
-                int randomIndex = new Random().nextInt(catBreeds.size());
+                int randomIndex = new Random().nextInt(wormTypes.size());
 
                 // Get a random name from the array
-                String newName = catBreeds.get(randomIndex);
+                String newName = wormTypes.get(randomIndex);
 
                 // Rename the user
                 guild.modifyNickname(member, newName).queue();
